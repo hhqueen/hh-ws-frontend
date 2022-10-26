c758f05ae857869c5f539701d8777bef56c23f57
bluz225 <bluz225@gmail.com>
2022-10-25 23:48:30 -0700

    📦 NEW: added immer and use-immer packages. Played with date and time package to transform time strings.

---
+++ src/helperFunctions/timeConverter.js
@@ -0,0 +1,17 @@
+// import date from "date-and-time"
+const date = require('date-and-time');
+const timeConverter = () =>{
+    const timeString = "21:45 PM"
+    const parsedHour = date.transform(timeString, "HH:mm A","H")
+    console.log(parsedHour)
+    const parsedMinute = date.transform(timeString, "HH:mm A","mm")
+    console.log(parsedMinute)
+    const parsedAMPM = date.transform(timeString, "HH:mm A","")
+    console.log(parsedAMPM)
+
+    const now = new Date()
+    const parsedNow = date.format(now,"hh:mm A")
+    console.log(parsedNow)
+}
+
+timeConverter()
