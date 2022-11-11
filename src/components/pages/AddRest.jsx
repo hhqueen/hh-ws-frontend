89680e725f8296ea9c5779d2b6eb9e97730c9b09
bluz225 <bluz225@gmail.com>
2022-11-10 18:26:57 -0800

    👌 IMPROVE: modified end2 default value to 23 instead of -1 which would cause a blank input

--- src/components/pages/AddRest.jsx
+++ src/components/pages/AddRest.jsx
@@ -39,13 +39,13 @@ const emptyRestaurantData = {
     hasPatio: false,
     cuisines: [],
     hours: [
-        { day: 0, hasHH1: true, start1: 15, end1: 18, end1close: false, hasHH2: false, start2: 22, end2: -1, end2close: false }, //monday
-        { day: 1, hasHH1: true, start1: 15, end1: 18, end1close: false, hasHH2: false, start2: 22, end2: -1, end2close: false }, //tuesday
-        { day: 2, hasHH1: true, start1: 15, end1: 18, end1close: false, hasHH2: false, start2: 22, end2: -1, end2close: false }, //weds
-        { day: 3, hasHH1: true, start1: 15, end1: 18, end1close: false, hasHH2: false, start2: 22, end2: -1, end2close: false }, // thurs
-        { day: 4, hasHH1: true, start1: 15, end1: 18, end1close: false, hasHH2: false, start2: 22, end2: -1, end2close: false }, //friday
-        { day: 5, hasHH1: true, start1: 15, end1: 18, end1close: false, hasHH2: false, start2: 22, end2: -1, end2close: false }, //sat
-        { day: 6, hasHH1: true, start1: 15, end1: 18, end1close: false, hasHH2: false, start2: 22, end2: -1, end2close: false }, //sun
+        { day: 0, hasHH1: true, start1: 15, end1: 18, end1close: false, hasHH2: false, start2: 21, end2: 23, end2close: false }, //monday
+        { day: 1, hasHH1: true, start1: 15, end1: 18, end1close: false, hasHH2: false, start2: 21, end2: 23, end2close: false }, //tuesday
+        { day: 2, hasHH1: true, start1: 15, end1: 18, end1close: false, hasHH2: false, start2: 21, end2: 23, end2close: false }, //weds
+        { day: 3, hasHH1: true, start1: 15, end1: 18, end1close: false, hasHH2: false, start2: 21, end2: 23, end2close: false }, // thurs
+        { day: 4, hasHH1: true, start1: 15, end1: 18, end1close: false, hasHH2: false, start2: 21, end2: 23, end2close: false }, //friday
+        { day: 5, hasHH1: true, start1: 15, end1: 18, end1close: false, hasHH2: false, start2: 21, end2: 23, end2close: false }, //sat
+        { day: 6, hasHH1: true, start1: 15, end1: 18, end1close: false, hasHH2: false, start2: 21, end2: 23, end2close: false }, //sun
     ],
     menu: {
         restaurantname: "",
