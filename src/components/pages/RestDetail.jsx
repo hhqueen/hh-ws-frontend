179ef4e196a746a331c5ef3fe51e18cfec7459f8
bluz225 <bluz225@gmail.com>
2022-11-27 21:49:27 -0800

    added site setting configuration and fixed doc pop for mongo data

--- src/components/pages/RestDetail.jsx
+++ src/components/pages/RestDetail.jsx
@@ -1,6 +1,10 @@
 import { useState, useEffect, Suspense } from 'react'
 import { useParams } from "react-router-dom"
 import showApplicableFilters from "../../helperFunctions/showApplicableFilters"
+import { siteSettings } from "../../sourceData/siteSettings"
+
+import { FaDirections } from "react-icons/fa"
+import { TbPhoneCall } from "react-icons/tb"
 
 import axios from "axios"
 
@@ -38,7 +42,7 @@ export default function RestDetail() {
         //   showApplicableFilters(response.data)
         // )
         await setAddress(`${response.data.address1} ${response.data.city} ${response.data.state} ${response.data.zip_code}`)
-        
+
         setIsloaded(true)
         // setIsloaded(true)
       } catch (error) {
@@ -46,7 +50,7 @@ export default function RestDetail() {
       }
     }
     getRestData()
-  },[id])
+  }, [id])
 
   const mapHours = restData.hours.map((hour, idx) => {
     return (
@@ -61,86 +65,126 @@ export default function RestDetail() {
   return (
 
     <>
-    {
-      isLoaded ?
-    
-      <div
-      className='mt-[60px]'
-      >
-        < img src={restData?.image_url} alt={restData?.name} />
-        <div
-          className='py-3'
-        >
-          <p>{restData?.name}</p>
-          <p>{restData?.cuisines.join(", ")}</p>
-          <p>{showApplicableFilters(restData)}</p>
-
-          <a 
-            href={`https://www.google.com/maps/place/${address.replace(" ","+")}`} 
-            target="_blank" 
-            rel="noopener noreferrer">
-            <p>{address}</p>
-          </a>
-          <a href={`tel:${restData?.telNumber}`}>{restData.displayNumber}</a>
-        </div>
-        {/* Hour Header */}
-        <div
-                    className='grid grid-cols-7 pl-3'
-                >
-                    <p
-                        className={`text-[11px] justify-items-start col-start-1 col-end-1 `}
-                    >Day</p>
-
-                    <p
-                        className={`text-[11px] justify-items-start flex mx-5 col-start-2 col-span-3`}
-                    >Happy Hour</p>
-
-                    <p
-                        className={`text-[11px] justify-items-start flex mx-5 col-start-5 col-span-3`}
-                    >Late Night</p>
-                </div>
-        <div
-          className='py-3'
-        >
-          {mapHours}
-        </div>
-
-        <div
-        >
-          <div
-            className='flex flex-col items-center justify-center py-3'>
-            <p
-              className='border-b'
-            >Food Menu</p>
-            <p
-              className='px-10 text-center'
-            >{restData?.menu.foodSpecialsDescription}</p>
-            <MenuItems
-              ItemsArr={restData?.menu.foodMenu}
-              menuType="Food"
-            />
-          </div>
+      {
+        isLoaded ?
 
           <div
-            className='flex flex-col items-center justify-center py-3'
+            className='mt-[60px] px-3'
           >
-            <p
-              className='border-b'
-            >Drink Menu</p>
-            <p
-              className='px-10 text-center'
-            >{restData?.menu.drinkSpecialsDescription}</p>
-            <MenuItems
-              ItemsArr={restData?.menu.drinkMenu}
-              menuType="Drink"
-            />
+            < img src={restData?.image_url} alt={restData?.name} />
+            <div
+              className='py-3'
+            >
+              <p>{restData?.name}</p>
+              <p>{restData?.cuisines.join(", ")}</p>
+              <p>{showApplicableFilters(restData)}</p>
+
+              <a
+                href={`https://www.google.com/maps/place/${address.replace(" ", "+")}`}
+                target="_blank"
+                className="flex"
+                rel="noopener noreferrer">
+                <FaDirections />
+                <p
+                  className='text-[blue] underline'
+                >{address}</p>
+              </a>
+              <a
+                href={`tel:${restData?.telNumber}`}
+                className="flex"
+              >
+                <TbPhoneCall />
+                <p
+                  className='text-[blue] underline'
+                >{restData.displayNumber}</p>
+              </a>
+            </div>
+            {/* Hour Header */}
+            <div
+              className='grid grid-cols-7 pl-3'
+            >
+              <p
+                className={`text-[11px] justify-items-start col-start-1 col-end-1 `}
+              >Day</p>
+
+              <p
+                className={`text-[11px] justify-items-start flex mx-5 col-start-2 col-span-3`}
+              >Happy Hour</p>
+
+              <p
+                className={`text-[11px] justify-items-start flex mx-5 col-start-5 col-span-3`}
+              >Late Night</p>
+            </div>
+            <div
+              className='py-3'
+            >
+              {mapHours}
+            </div>
+
+            <div
+            >
+              <div
+                className='flex flex-col items-center justify-center py-3'>
+                <p
+                  className='border-b'
+                >Food Menu</p>
+                {
+                  siteSettings.showMenuImg ?
+                    <>
+                      <img
+                        src={`${restData.menu.foodMenuImg?.imgUrl}`}
+                        alt="image"
+                      />
+                    </>
+                    :
+                    <>
+                      <p
+                        className='px-10 text-center'
+                      >{restData?.menu.foodSpecialsDescription}</p>
+                      <MenuItems
+                        ItemsArr={restData?.menu.foodMenu}
+                        menuType="Food"
+                      />
+                    </>
+                }
+
+              </div>
+
+              <div
+                className='flex flex-col items-center justify-center py-3'
+              >
+                <p
+                  className='border-b'
+                >Drink Menu</p>
+
+                {
+                  siteSettings.showMenuImg ?
+                    <>
+                      <img
+                        src={`${restData.menu.drinkMenuImg?.imgUrl}`}
+                        alt="image"
+                      />
+                    </>
+                    :
+                    <>
+                      <p
+                        className='px-10 text-center'
+                      >{restData?.menu.drinkSpecialsDescription}</p>
+                      <MenuItems
+                        ItemsArr={restData?.menu.drinkMenu}
+                        menuType="Drink"
+                      />
+                    </>
+                }
+
+
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
 
-      :
+          :
 
-      <LoadingComp/>
+          <LoadingComp />
       }
     </>
   )
---
+++ src/sourceData/siteSettings.js
@@ -0,0 +1,3 @@
+export const siteSettings = {
+    showMenuImg: true
+}
