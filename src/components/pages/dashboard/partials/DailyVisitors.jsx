5c0a66a89432840d6dcf9bd86dc9f39cf46a8c63
Billy <bluz225@gmail.com>
2023-02-21 18:04:53 -0800

    added daily visitors and weekly email subs to dashboard

--- src/components/pages/dashboard/partials/DailyVisitors.jsx
+++ src/components/pages/dashboard/partials/DailyVisitors.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
 
+const headers = [
+  "Daily Vistors", "Web", "Mobile", "registered", "unregistered"
+]
+
+const data1 = [
+  "Average Last 7 Days", 10, 10, 10, 10
+]
+
+const data2 = [
+  "Average Last 30 Days", 10, 10, 10, 10
+]
+
 export default function DailyVisitors() {
+
+
+  const renderData = (array) => {
+      return array.map((item)=>{
+        return <p>{item}</p>
+      })
+  } 
+
+  const renderHeaders = renderData(headers)
+  const renderData1 = renderData(data1)
+  const renderData2 = renderData(data2)
+
+
   return (
-    <div>DailyVisitors</div>
+    <div
+      className='grid grid-cols-5 text-center border rounded-sm'
+    >
+      {renderHeaders}
+      {renderData1}
+      {renderData2}
+
+    </div>
   )
 }
